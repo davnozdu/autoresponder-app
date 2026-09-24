160c08de49d3fdca0866c18533dbebe570bd83af
davnozdu <davnozdu@gmail.com>
2026-09-24 16:36:23 +0200

    v0.21.0: versionCode 97 (скрининг звонков + фиксы аудита)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.davnozdu.autoresponder"
         minSdk = 29
         targetSdk = 35
-        versionCode = 96
-        versionName = "0.20.0"
+        versionCode = 97
+        versionName = "0.21.0"
     }
 
     signingConfigs {
